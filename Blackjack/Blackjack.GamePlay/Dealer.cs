026f74f7cd60762f2d5696737733ccee4739aa40
Vera-Alejandro <Alejandro.Vera-Gonzalez@trojans.dsu.edu>
2021-05-20 21:53:07 -0500

    new dealer class

---
+++ Blackjack/Blackjack.GamePlay/Dealer.cs
@@ -0,0 +1,15 @@
+﻿namespace Blackjack.GamePlay
+{
+    public class Dealer
+    {
+        public int Id { get; set; }
+        public Hand CurrentHand { get; set; }
+
+
+        public Dealer()
+        {
+            CurrentHand = new Hand();
+        }
+
+    }
+}
