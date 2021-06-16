685664231177e5b26f12e2e61b8285dcf78bb6fe
Alejandro Vera <averagonzalez@omnitech-inc.com>
2021-06-16 17:01:25 -0500

    added interface and testing classes

---
+++ Blackjack/CoreGameTesting/GameInstanceTesting.cs
@@ -0,0 +1,42 @@
+﻿using Blackjack.Data.Enums;
+using Blackjack.GamePlay;
+using NUnit.Framework;
+
+namespace CoreGameTesting
+{
+    public class GameInstanceTesting
+    {
+        public GameInstance TestGame { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            TestGame = new GameInstance();
+        }
+
+        [Test]
+        public void Win_Endgame_Payout_Success()
+        {
+            TestGame.BetIfPossible(100f);
+
+            TestGame.Payout(GameResult.Win);
+
+            var playerCurrentCash = TestGame.GetPlayerCash();
+
+            Assert.AreEqual(600f, playerCurrentCash);
+        }
+
+        [Test]
+        public void BlackJackWin_Endgame_Payout()
+        {
+            TestGame.BetIfPossible(100f);
+
+            TestGame.Payout(GameResult.PlayerBlackjack);
+
+            var playerCurrentCash = TestGame.GetPlayerCash();
+
+            Assert.AreEqual(650f, playerCurrentCash);
+        }
+    }
+}
+
